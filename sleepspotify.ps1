01ce0660449236a68747b5a69874cb1375075c4d
Michael Purdy <0mpurdy@gmail.com>
2017-12-04 00:21:19 +0000

    Make time variable on sleepspotify

--- sleepspotify.ps1
+++ sleepspotify.ps1
@@ -1,5 +1,10 @@
+# This is a script to stop spotify late at night after going to sleep
+
+# Choose the time to stop spotify
+$bedtime = Read-Host -Prompt "What time do you wish to close Spotify? (HH:MM)"
+
 # Create a task to kill spotify at a certain time
-schtasks.exe /create /TN sleepspotify /sc once /ST 00:15 /tr "taskkill /im Spotify.exe"
+schtasks.exe /create /TN sleepspotify /sc once /ST $bedtime /tr "taskkill /im Spotify.exe"
 
 # Print out the list of tasks to double check it has worked
 schtasks.exe /query | grep -A 4 -B 4 spot
